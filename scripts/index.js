600a2dad80e0a6cf2995019f9ce657c51d8dd8b9
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-03 18:20:07 +0500

    Add styles. Add function for a to-do list.

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,18 @@
-let textInput = document.getElementById('text-input');
-let submitButton =
+function createNewTask() {
+    let textInput = document.getElementById('text-input');
+    let submitButton = document.getElementById('text-btn');
+    let areaForText = document.getElementById('inner-area');
+    submitButton.addEventListener('click', () => {
+        let anyTask = document.createElement('div');
+        anyTask.classList.add('input-inner__task');
+        if (textInput.value) {
+            anyTask.textContent = textInput.value;
+            areaForText.appendChild(anyTask);
+        } else if (textInput.value == '' || textInput.value == null || textInput.value == ' ') {
+            console.log("Empty");
+        }
+        textInput.value = '';
+    });
+}
+
+createNewTask();
