e838dd3d78d0dc99054a02008df0908de201912b
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-04 17:13:20 +0500

    Add controls. Correct styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,40 @@
+/*******************
+ * TODO:
+ *  - Submit task on enter press
+ *  - Fix lines wrapping
+ */
+
 function createNewTask() {
+    /************************
+    Comments need to be made!!!
+    *************************/
+
+    /*
+    Descripiton of variables
+    */
     let textInput = document.getElementById('text-input');
     let submitButton = document.getElementById('text-btn');
     let areaForText = document.getElementById('inner-area');
+
+    /*
+    What happens here?
+    */
     submitButton.addEventListener('click', () => {
         let anyTask = document.createElement('div');
         anyTask.classList.add('input-inner__task');
         if (textInput.value) {
+            //create text without controls
             anyTask.textContent = textInput.value;
             areaForText.appendChild(anyTask);
+
+            function createControls(type, symbol) {
+                let typeControl = document.createElement('span');
+                typeControl.classList.add(`input-inner__${type}task`);
+                typeControl.innerHTML = `${symbol}`;
+                anyTask.appendChild(typeControl);
+            }
+            createControls('done', "&#10004");
+            createControls('delete', "&#10008");
         } else if (textInput.value == '' || textInput.value == null || textInput.value == ' ') {
             console.log("Empty");
         }
@@ -15,4 +42,5 @@ function createNewTask() {
     });
 }
 
+// Is this a write way to call function after page loading?
 createNewTask();
