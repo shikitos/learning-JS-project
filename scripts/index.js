4bfbd4a8558695e4896d405f9ed8ae9aa7515500
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-10 20:05:33 +0500

    Fix bug with appending

--- scripts/index.js
+++ scripts/index.js
@@ -11,69 +11,80 @@ function createNewTask() {
     //array with tasks
     let todoList = [];
 
-    let temp = {};
 
-    //create eventListenere for the button
+    //create eventListener for the button
     submitButton.addEventListener('click', () => {
-        //create new <div></div>
-        anyTask = document.createElement('div');
-        temp.todo = textInput.value;
-        temp.check = false;
-        todoList.push(temp);
-        areaForText.appendChild(anyTask);
-        console.log(todoList);
-
-        outputElements();
-
-        function outputElements() {
-            //give it the class
+        if (textInput.value !== '' || textInput.value == null) {
+            let temp = {};
+            //create new <div></div>
+            let anyTask = document.createElement('div');
             anyTask.classList.add('input-inner__task');
-            //clear the value
-            anyTask.value = '';
-            for (let i = 0; i < todoList.length; i++) {
-                areaForText.innerHTML +=
-                    '<input type="checkbox" checked> ' + todoList[i].todo;
+            temp.todo = textInput.value;
+            temp.check = false;
+            todoList.push(temp);
+            areaForText.appendChild(anyTask);
+            console.log(todoList);
+
+            outputElements();
+
+            function outputElements() {
+                //give it the class
+                //clear the value
+                textInput.value = '';
+                for (let i = 0; i < todoList.length; i++) {
+                    if (todoList[i].check) {
+                        anyTask.innerHTML +=
+                            `<span class="donetask"><input type="checkbox" checked> ${todoList[i].todo}</span><br>`;
+                    }
+                    else {
+                        anyTask.innerHTML +=
+                            `<span><input type="checkbox"> ${todoList[i].todo}</span><br>`;
+                    }
+                }
             }
+
+            //     //if input not empty
+            //     if (textInput.value) {
+            //         //create text without controls
+            //         anyTask.textContent = textInput.value;
+            //         //add text at the end of the div
+            //         areaForText.appendChild(anyTask);
+            //         //add task to the arr
+            //         tasksArr.push(anyTask);
+
+            //         //create controls function
+            //         function createControls(type, symbol) {
+            //             //create new <span></span>
+            //             let typeControl = document.createElement('span');
+            //             //give it the class
+            //             typeControl.classList.add(`input-inner__${type}task`);
+            //             //give it the content
+            //             typeControl.innerHTML = symbol;
+            //             //add controls at the end of the div
+            //             anyTask.appendChild(typeControl);
+            //             //add controls to the arr
+            //             controlsArr.push(typeControl);
+            //         }
+            //         //create for "done" element
+            //         createControls('done', '&#10004');
+            //         //create for "delete" element
+            //         createControls('delete', '&#10008');
+            //         //else input is empty
+            //     } else if (
+            //         textInput.value == '' ||
+            //         textInput.value == null ||
+            //         textInput.value == ' '
+            //     ) {
+            //         console.log('Empty');
+            //     }
+            //     //clear the input after click
+            //     textInput.value = '';
+            //     // doneTask();
+            //     // deleteTask();
         }
-
-        //     //if input not empty
-        //     if (textInput.value) {
-        //         //create text without controls
-        //         anyTask.textContent = textInput.value;
-        //         //add text at the end of the div
-        //         areaForText.appendChild(anyTask);
-        //         //add task to the arr
-        //         tasksArr.push(anyTask);
-
-        //         //create controls function
-        //         function createControls(type, symbol) {
-        //             //create new <span></span>
-        //             let typeControl = document.createElement('span');
-        //             //give it the class
-        //             typeControl.classList.add(`input-inner__${type}task`);
-        //             //give it the content
-        //             typeControl.innerHTML = symbol;
-        //             //add controls at the end of the div
-        //             anyTask.appendChild(typeControl);
-        //             //add controls to the arr
-        //             controlsArr.push(typeControl);
-        //         }
-        //         //create for "done" element
-        //         createControls('done', '&#10004');
-        //         //create for "delete" element
-        //         createControls('delete', '&#10008');
-        //         //else input is empty
-        //     } else if (
-        //         textInput.value == '' ||
-        //         textInput.value == null ||
-        //         textInput.value == ' '
-        //     ) {
-        //         console.log('Empty');
-        //     }
-        //     //clear the input after click
-        //     textInput.value = '';
-        //     // doneTask();
-        //     // deleteTask();
+        else {
+            console.log("Empty string");
+        }
     });
 }
 
