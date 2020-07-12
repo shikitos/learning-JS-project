5f96ac51259ff02c72f1411b0d91969dc83e41ec
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-12 19:33:23 +0500

    Add localStorage. Need to fix reoutput every clicking on todo btn

--- scripts/index.js
+++ scripts/index.js
@@ -6,60 +6,97 @@ function createNewTask() {
     let submitButton = document.getElementById('text-btn');
     //get text output area from dom and create var of the area
     let areaForText = document.getElementById('inner-area');
-    // //array with controls
-    // let controlsArr = [];
-    //array with tasks
+    //create array with tasks
     let todoList = [];
+
 
 
     //create eventListener for the button
     submitButton.addEventListener('click', () => {
         if (textInput.value !== '' || textInput.value == null) {
+            //create temporary object
             let temp = {};
-            //create new <div></div>
-            let anyTask = document.createElement('div');
-            //git class to the element
-            anyTask.classList.add('input-inner__task');
-            //add value to the array todo
+            //add value to the array todoList
             temp.todo = textInput.value;
             //add check false (for true in the future when task has done)
             temp.check = false;
             //add temporary array to the current
             todoList.push(temp);
-            //add new div into areaForText
-            areaForText.appendChild(anyTask);
-            //start outputing elemtns
-            function outputElements() {
-                //give it the class
-                //clear the value
-                textInput.value = '';
-                //outputText field - create and make it clean
-                let outputText = '';
-                //start cycle for array
-                for (let i = 0; i < todoList.length; i++) {
-                    //if element is checked
-                    if (todoList[i].check) {
-                        //we will add class to the span
-                        outputText =
-                            `<span class="donetask"><input type="checkbox" checked>${todoList[i].todo}</span><br>`;
-                    }
-                    //or, if it isn't checked
-                    else {
-                        outputText =
-                            `<span><input type="checkbox"> ${todoList[i].todo}</span><br>`;
-                    }
-                }
-                //output current text at the next line
-                anyTask.innerHTML = outputText;
-            }
-            //output text function
+            //start outputting elements
             outputElements();
+            //localStorage for the todoList
+            localStorage.setItem('todoList', JSON.stringify(todoList));
         //if value is empty
         } else {
             console.log("Empty string");
         }
     });
+
+    //function for outputElements from todoList
+    function outputElements() {
+        // console.log("function is working");
+        // //give it the class
+        // //clear the value
+        // textInput.value = '';
+        // //outputText field - create and make it clean
+        // let outputText = '';
+        // //start cycle for array
+        // for (let i = 0; i < todoList.length; i++) {
+        //     //if element is checked
+        //     if (todoList[i].check) {
+        //         //we will add class to the span
+        //         outputText =
+        //             `<span class="donetask"><input type="checkbox" checked>${todoList[i].todo}</span><br>`;
+        //         console.log("Working");
+        //     }
+        //     //or, if it isn't checked
+        //     else {
+        //         outputText =
+        //             `<span><input type="checkbox"> ${todoList[i].todo}</span><br>`;
+        //     }
+        // }
+        //output current text at the next line
+        todoList.forEach(elem => {
+            if (elem.check) {
+                //create new <div></div>
+                let anyTask = document.createElement('div');
+                //git class to the element
+                anyTask.classList.add('input-inner__task');
+                //add new div into areaForText
+                areaForText.appendChild(anyTask);
+                anyTask.innerHTML += `<span class="donetask"><input type="checkbox" checked>${elem.todo}</span>`;
+            } else{
+                //create new <div></div>
+                let anyTask = document.createElement('div');
+                //git class to the element
+                anyTask.classList.add('input-inner__task');
+                //add new div into areaForText
+                areaForText.appendChild(anyTask);
+                anyTask.innerHTML += `<span><input type="checkbox"> ${elem.todo}</span>`;
+            }
+        });
+    }
+
+    //delete tasks
+    function deleteTasks() {
+        let btnDeleteTask = document.getElementById('delete-tasks');
+
+        btnDeleteTask.addEventListener('click', () => {
+            for (let key in todoList) {
+                todoList.splice(key, 1);
+            }
+        })
+    }
+    deleteTasks();
+
+    if (localStorage.getItem('todoList')) {
+        todoList = JSON.parse(localStorage.getItem('todoList'));
+        outputElements();
+    }
 }
 
 //new task function
 createNewTask();
+
+
+
