947575454343688ffe6af6359f9a0539061a7405
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-18 19:46:18 +0500

    Add bad output doneTasks. Fix bugs with checkbox todoList.check prop

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ function createNewTask() {
     let unorderedList = document.getElementById('place');
     //create array with tasks
     let todoList = [];
-
+    //create var with date object
     let date = new Date();
 
     //create eventListener for the button
@@ -20,12 +20,12 @@ function createNewTask() {
             temp.todo = textInput.value;
             //add check false (for true in the future when task has done)
             temp.check = false;
+            //add date for the day when you create the task
             temp.date = {
                 day: date.getDate(),
                 month: date.getMonth() + 1,
                 year: date.getFullYear(),
             }
-            console.log(temp.date);
             //add temporary array to the current
             todoList[todoList.length] = temp;
             //start outputting elements
@@ -43,11 +43,15 @@ function createNewTask() {
         //clear the output
         let output = '';
         //start iterating through an array
-        for (let key in todoList) {
+        for (let key = 0; key < todoList.length; key++) {
             //constant for a shorter entry
-            const { todo, check, date } = todoList[key];
+            const { todo, date } = todoList[key];
             // if task is checked
-            output += `<li class="input-inner__task"><input type="checkbox" class="checkbox${key}"><span class="task-todo">${todo}</span><span class="task-date">  ${date.day}/${date.month}/${date.year}</span></li>`;
+            if (todoList[key].check) {
+                output += `<li class="input-inner__task donetask"><input checked type="checkbox" class="checkbox${key}"><span class="task-todo">${todo}</span><span class="task-date">  ${date.day}/${date.month}/${date.year}</span></li>`;
+            } else {
+                output += `<li class="input-inner__task"><input type="checkbox" class="checkbox${key}"><span class="task-todo">${todo}</span><span class="task-date">  ${date.day}/${date.month}/${date.year}</span></li>`;
+            }
         }
         //output all tasks to the HTML
         unorderedList.innerHTML = output;
@@ -59,6 +63,7 @@ function createNewTask() {
         checkItem();
         //start function which will move and delete from list done tasks
         clearAndMoveDoneTasks();
+        outputDoneTasks();
         //clear the input.value section
         textInput.value = '';
     }
@@ -68,11 +73,11 @@ function createNewTask() {
         //get li elements
         let innerTask = unorderedList.children;
         //iterating through an array of tasks
-        for (let key in todoList) {
+        for (let key = 0; key < todoList.length; key++) {
             //get checkbox of the every task
             let checkbox = document.querySelector(`.checkbox${key}`);
             //add event for every checkbox
-            checkbox.addEventListener('click', () => {
+            checkbox.addEventListener('change', () => {
                 //toggle class at the clicked checkbox
                 // innerTask[key].classList.toggle('donetask');
                 /*
@@ -80,10 +85,10 @@ function createNewTask() {
                 **when click at the check box
                 ** class toggle true <-> false
                  */
-                if (!todoList.check) {
+                if (!todoList[key].check) {
                     innerTask[key].classList.add('donetask');
                     //toggle check:false to true
-                    todoList.check = true;
+                    todoList[key].check = true;
                     //get elements from local storage
                     let localArr = JSON.parse(localStorage.getItem('todoList'));
                     //toggle check at the local storage to true
@@ -93,7 +98,7 @@ function createNewTask() {
                 } else {
                     innerTask[key].classList.remove('donetask');
                     //toggle check:false to false
-                    todoList.check = false;
+                    todoList[key].check = false;
                     //get elements from local storage
                     let localArr = JSON.parse(localStorage.getItem('todoList'));
                     //toggle check at the local storage to false
@@ -105,22 +110,51 @@ function createNewTask() {
         }
     }
 
-    //todo too much clicks = to much output
     function clearAndMoveDoneTasks() {
-        let textArea = document.getElementById('donetask')
+        let textArea = document.getElementById('donetask');
         let li = unorderedList.children;
         let btnClearDoneTasks = document.getElementById('done-tasks');
         btnClearDoneTasks.addEventListener('click', () => {
+            textArea.innerHTML = '';
             for (let i = 0; i < li.length; i++) {
                 // while (li[i] && li[i].children[0].checked) {
-                    textArea.innerHTML += `<li class="footer__datalist">${todoList[i].todo}</li>`;
+                if (li[i].children[0].checked) {
+                    if (localStorage.getItem('doneTasks')) {
+                        let doneTasksArr = JSON.parse(localStorage.getItem('doneTasks'));
+                        console.log(doneTasksArr);
+                        doneTasksArr.push(todoList[i]);
+                        // doneTasksArr.push(todoList[i]);
+                        localStorage.setItem('doneTasks', JSON.stringify(doneTasksArr));
+                        textArea.innerHTML += `<li class="footer__datalist"><span class="task-done">${todoList[i].todo}</span><span class="task-donedate">${todoList[i].date.day}/${todoList[i].date.month}/${todoList[i].date.year}</span></li>`;
+                        unorderedList.removeChild(li[i]);
+                        //extract an array from the local storage
+                        let localArr = JSON.parse(localStorage.getItem('todoList'));
+                        //delete this element
+                        localArr.splice(i, 1);
+                        //save array to the local storage
+                        localStorage.setItem('todoList', JSON.stringify(localArr));
+                        outputDoneTasks();
+                    }
+                }
+                 else {
+                    console.log("Unchecked");
+                }
                 // }
                 // textArea.innerHTML += + ' ';
             }
         });
     }
 
-    //todo when I click at the button — delete first task, not checked
+    function outputDoneTasks() {
+        let textArea = document.getElementById('donetask');
+        if (localStorage.getItem('doneTasks')) {
+            let localDoneTasks = JSON.parse(localStorage.getItem('doneTasks'));
+            for (let i = 0; i < localDoneTasks.length; i++) {
+                textArea.innerHTML += `<li class="footer__datalist"><span class="task-done">${localDoneTasks[i].todo}</span><span class="task-donedate">${localDoneTasks[i].date.day}/${localDoneTasks[i].date.month}/${localDoneTasks[i].date.year}</span></li>`;
+            }
+        }
+    }
+
     //delete checked tasks
     function deleteTasks() {
         //get list of tasks
@@ -139,7 +173,6 @@ function createNewTask() {
                     //extract an array from the local storage
                     let localArr = JSON.parse(localStorage.getItem('todoList'));
                     //delete this element
-                    console.log(localArr);
                     localArr.splice(i, 1);
                     //save array to the local storage
                     localStorage.setItem('todoList', JSON.stringify(localArr));
@@ -175,6 +208,3 @@ function createNewTask() {
 
 //new task function
 createNewTask();
-
-
-
